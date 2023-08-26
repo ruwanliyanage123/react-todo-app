da3f9e0516c202aece40712ef02c2ba853633088
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-08-26 15:52:25 +0530

    Add modal for the site

--- src/components/dashbord/Dashboard.js
+++ src/components/dashbord/Dashboard.js
@@ -1,60 +1,34 @@
 import { useState } from 'react';
 import './Dashboard.css';
-import Modal from './Modal';
+import React from 'react';
+import Modal from 'react-modal';
+import TodoModal from './TodoModal';
 
 const Dashboard = ()=>{
-    const [weight, setWeight] = useState('');
-    const [height, setHeight] = useState('');
-    const [result, setResult] = useState('Enter values');
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+
+    function openModal() {
+      setIsOpen(true);
+    }
+  
+    function afterOpenModal() {
+      
+    }
+  
+    function closeModal() {
+      setIsOpen(false);
+    }
 
     return(
         <div className='dashboard'>
-            <div className='content-table'>
-                <table>
-                    <tr>               
-                        <th></th>
-                        <th></th>
-                    </tr>
-                    <tr>
-                        <td>
-                            Result:
-                        </td>
-                        <td>
-                            <h1>{result}</h1>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Weight:
-                        </td>
-                        <td>
-                            <input type= "number" value={weight} onChange={(e)=> setWeight(e.target.value)}/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Height:
-                        </td>
-                        <td>
-                            <input type="number" value={height} onChange={(e)=> setHeight(e.target.value)}/>  
-                        </td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>
-                            <button onClick={whenSubmit}>Submit</button>
-                        </td>
-                    </tr>
-                </table>
-            </div>  
+            <div className='button-panel'>
+                <button onClick={openModal}>Open Modal</button>
+                <Modal isOpen={modalIsOpen} className='modal' onAfterOpen={afterOpenModal} onRequestClose={closeModal} >
+                    <TodoModal closeModal={closeModal} />
+                </Modal>
+            </div>
         </div>
     );
-
-    function whenSubmit(){
-        var numb = parseInt(weight) * parseInt(height);
-        console.log(numb);
-        setResult(numb);
-    }
 }
 
 export default Dashboard;
