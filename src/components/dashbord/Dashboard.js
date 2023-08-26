1e12cdb577222a9775081bfed78e54717f9b8910
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-08-26 18:30:31 +0530

    reset code

--- src/components/dashbord/Dashboard.js
+++ src/components/dashbord/Dashboard.js
@@ -1,31 +1,11 @@
-import { useState } from 'react';
 import './Dashboard.css';
-import React from 'react';
-import Modal from 'react-modal';
-import TodoModal from './TodoModal';
 
 const Dashboard = ()=>{
-    const [modalIsOpen, setIsOpen] = React.useState(false);
-
-    function openModal() {
-      setIsOpen(true);
-    }
-  
-    function afterOpenModal() {
-      
-    }
-  
-    function closeModal() {
-      setIsOpen(false);
-    }
 
     return(
         <div className='dashboard'>
             <div className='button-panel'>
-                <button onClick={openModal}>Open Modal</button>
-                <Modal isOpen={modalIsOpen} className='modal' onAfterOpen={afterOpenModal} onRequestClose={closeModal} >
-                    <TodoModal closeModal={closeModal} />
-                </Modal>
+                
             </div>
         </div>
     );
